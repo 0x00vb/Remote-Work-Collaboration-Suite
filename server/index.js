e70aa9c926b93714d160ecbf3d43b5e65781af62
valentinoB <valentinobalatti4@gmail.com>
2024-04-21 23:55:18 -0300

    Files for chat api

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const socketio = require('socket-io');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth.routes');
+const messagesRouter = require('./routes/messages.routes');
 
 const app = express();
+const server = require('http').Server(app);
+const io = socketio(server);
 
 app.use(cors({origin: '*', credentials: true, methods: ['GET', 'POST', 'DELETE']}))
 app.use(cookieParser())
@@ -20,7 +24,20 @@ mongoose.connect(process.env.MONGO_URI)
     console.log("[!] Failed to connect! ", e.message)
 })
 
-app.use('/auth', authRoutes)
+io.on('connection', (socket) => {
+    console.log(`Socket ${socket.id} connected`);
+  
+    socket.on('sendMessage', (message) => {
+      io.emit('message', message);
+    });
+  
+    socket.on('disconnect', () => {
+      console.log(`Socket ${socket.id} disconnected`);
+    }); 
+});
+
+app.use('/auth', authRoutes);
+app.use('/messages', messagesRouter);
 
 app.listen(process.env.PORT, () => {
     console.log('[+] Listening on http://localhost:' + process.env.PORT)
